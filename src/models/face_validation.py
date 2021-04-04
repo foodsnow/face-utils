90b75d43550ec32d2d7cc6099d7975814aca180a
foodsnow <dut.kht@gmail.com>
2021-04-04 18:43:09 +0600

    passport image sharpening before embedding

--- src/models/face_validation.py
+++ src/models/face_validation.py
@@ -1,18 +1,33 @@
 from facenet_pytorch import MTCNN, InceptionResnetV1
-from src.models.utils import cosine_simularity
+from src.models.utils import cosine_simularity, sharpen_image
 
 
 class DocumentFaceChecker:
 
     def __init__(self):
-        self.mtcnn = MTCNN(keep_all=True, min_face_size=10, image_size=200)
+        self.mtcnn = MTCNN(
+            keep_all=True,
+            min_face_size=30,
+            image_size=200
+        )
         self.embedding = InceptionResnetV1(pretrained='vggface2').eval()
 
     def check(self, img_RGB):
-        faces = self.mtcnn(img_RGB)
+        boxes, probs = self.mtcnn.detect(img_RGB)
 
-        if faces.shape[0] < 2:
+        if probs.shape[0] < 2:
             raise Exception("couldn't find two faces")
+
+        sorted_by_area = sorted(
+            boxes,
+            key=lambda box: (box[1] - box[3]) * (box[0] - box[2]),
+            reverse=True
+        )
+        faces = self.mtcnn.extract(img_RGB, sorted_by_area, None)
+        print(faces[1].shape)
+        sharpened_image = sharpen_image(faces[1])
+        print(sharpened_image.shape)
+        faces[1] = sharpened_image
 
         print(faces.shape)
         vector_faces = self.embedding(faces)[:2]
--- src/models/utils.py
+++ src/models/utils.py
@@ -1,6 +1,17 @@
 from numpy import dot
 from numpy.linalg import norm
+import numpy as np
+import torch
+import cv2
 
 
 def cosine_simularity(a, b):
     return dot(a, b) / (norm(a) * norm(b))
+
+
+def sharpen_image(image):
+    np_image = image.permute(1, 2, 0).numpy()
+    kernel = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
+    sharpened_image = cv2.filter2D(np_image, -1, kernel)
+    tensor_img = torch.from_numpy(sharpened_image)
+    return tensor_img.permute(2, 0, 1)
